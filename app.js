688969971a67aa7ccbbaa2a5e08b4b41776e760a
Swathi <swathiasok14@gmail.com>
2023-05-09 23:12:19 +0530

    Modified the blog contents and added new database

--- app.js
+++ app.js
@@ -1,90 +1,73 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const mongoose = require('mongoose');
 const _ = require('lodash');
-const app = express();
-
+const mongoose = require('mongoose');
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({extended : true}));
+require("dotenv").config({ path: path.resolve(__dirname, '../.env') });
+
+const app = express();
+
 app.set('view engine', 'ejs');
 app.use('/public/', express.static('./public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
-
-mongoose.connect("mongodb+srv://swathiasok:swathi14@cluster0.ak60v.mongodb.net/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://swathiasok14:swaash143@blog1.fdxanvv.mongodb.net/blogPostDB",{useNewUrlParser: true, useUnifiedTopology: true});
 
 const postSchema = {
-
   title: String,
-  body: String,
   author: String,
-  img: Buffer
- 
- };
+  image_url: String,
+  body: String
+}
 
-const Post = mongoose.model("Post",postSchema);
+const Post = mongoose.model("Post", postSchema);
 
-app.get("/posts/:postId", function (req, res) {
-  const requestedPostId = req.params.postId;
-  Post.findOne({_id: requestedPostId}, function(err, post){ 
-    res.render("post", { title : post?.title, body : post?.body, author: post?.author, img: post?.img });
+app.get("/", function (req, res) {
+  Post.find({}, function(err, posts){
+    res.render("home", {posts : posts.slice(-3)});
+ })
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.get("/posts", function (req, res) {
+  Post.find({}, function(err, posts){
+    res.render("posts", {posts : posts});
+ })
+});
+
+app.post("/", function (req, res) {
+  res.redirect("/posts");
+});
+
+app.post("/compose", function (req, res) {
+  const post = new Post({
+    title: req.body.title,
+    author: req.body.author,
+    image_url: req.body.image,
+    body: req.body.post
+  });
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
   });
 });
 
-app.get("/", function(req, res){
-  Post.find({}, function(err, posts){
-    res.render("home", {posts});
- })
-})
+app.get("/posts/:postId", function (req, res) {
 
-app.get("/posts", function(req,res)
-{
-  Post.find({}, function(err, posts){
-    res.render("posts", {posts});
- })
-})
+  const requestedPostId = req.params.postId;
+  Post.findOne({_id: requestedPostId}, function(err, post){ 
+    res.render("post", { title : post?.title, body : post?.body, author: post?.author, img: post?.image_url });
+  });
+});
 
-app.post("/",function(req,res)
-  {
-    res.redirect("/posts");
-  })
-  
-  app.get("/compose", function(req, res){
-    res.render("compose");
-  })
-
-  app.post("/posts/:postId",function(req,res)
-  {
-    res.redirect("/");
-  })
-
-  app.post("/posts",function(req,res)
-  {
-    res.redirect("/");
-  })
-
-  app.post("/compose", function(req, res){
-      const post = new Post({
-        title : req.body.titleText,
-        body : req.body.postText,
-        author : req.body.authorName, 
-        img : req.body.img
-      });
-      post.save(function(){
-        res.redirect("/");
-      });
-
-  })
-  
-
-
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 5000;
-}
-
-app.listen(port, function(){
-  console.log("Server is running on port 5000");
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Server is running!');
 })
